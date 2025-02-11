3ac0391184950b1567b941436541009146d2103a
dwinkler1 <dwinkler1@users.noreply.github.com>
2025-02-11 16:12:27 +0000

    Commit from GitHub Actions (Recreate R flakes)

--- templates.nix
+++ templates.nix
@@ -1,4 +1,256 @@
 {
+  r-2019-03-14 = {
+    path = ./templates/2019-03-14;
+    description = "2019-03-14";
+  };
+  r-2019-05-05 = {
+    path = ./templates/2019-05-05;
+    description = "2019-05-05";
+  };
+  r-2019-07-22 = {
+    path = ./templates/2019-07-22;
+    description = "2019-07-22";
+  };
+  r-2019-12-19 = {
+    path = ./templates/2019-12-19;
+    description = "2019-12-19";
+  };
+  r-2020-03-12 = {
+    path = ./templates/2020-03-12;
+    description = "2020-03-12";
+  };
+  r-2020-04-27 = {
+    path = ./templates/2020-04-27;
+    description = "2020-04-27";
+  };
+  r-2020-06-22 = {
+    path = ./templates/2020-06-22;
+    description = "2020-06-22";
+  };
+  r-2020-08-20 = {
+    path = ./templates/2020-08-20;
+    description = "2020-08-20";
+  };
+  r-2020-10-30 = {
+    path = ./templates/2020-10-30;
+    description = "2020-10-30";
+  };
+  r-2021-02-26 = {
+    path = ./templates/2021-02-26;
+    description = "2021-02-26";
+  };
+  r-2021-04-01 = {
+    path = ./templates/2021-04-01;
+    description = "2021-04-01";
+  };
+  r-2021-05-29 = {
+    path = ./templates/2021-05-29;
+    description = "2021-05-29";
+  };
+  r-2021-08-03 = {
+    path = ./templates/2021-08-03;
+    description = "2021-08-03";
+  };
+  r-2021-10-28 = {
+    path = ./templates/2021-10-28;
+    description = "2021-10-28";
+  };
+  r-2022-01-16 = {
+    path = ./templates/2022-01-16;
+    description = "2022-01-16";
+  };
+  r-2022-04-19 = {
+    path = ./templates/2022-04-19;
+    description = "2022-04-19";
+  };
+  r-2022-06-22 = {
+    path = ./templates/2022-06-22;
+    description = "2022-06-22";
+  };
+  r-2022-08-22 = {
+    path = ./templates/2022-08-22;
+    description = "2022-08-22";
+  };
+  r-2022-10-20 = {
+    path = ./templates/2022-10-20;
+    description = "2022-10-20";
+  };
+  r-2022-12-20 = {
+    path = ./templates/2022-12-20;
+    description = "2022-12-20";
+  };
+  r-2023-02-13 = {
+    path = ./templates/2023-02-13;
+    description = "2023-02-13";
+  };
+  r-2023-04-01 = {
+    path = ./templates/2023-04-01;
+    description = "2023-04-01";
+  };
+  r-2023-06-15 = {
+    path = ./templates/2023-06-15;
+    description = "2023-06-15";
+  };
+  r-2023-08-15 = {
+    path = ./templates/2023-08-15;
+    description = "2023-08-15";
+  };
+  r-2023-10-30 = {
+    path = ./templates/2023-10-30;
+    description = "2023-10-30";
+  };
+  r-2023-12-30 = {
+    path = ./templates/2023-12-30;
+    description = "2023-12-30";
+  };
+  r-2024-02-29 = {
+    path = ./templates/2024-02-29;
+    description = "2024-02-29";
+  };
+  r-2024-04-29 = {
+    path = ./templates/2024-04-29;
+    description = "2024-04-29";
+  };
+  r-2024-06-14 = {
+    path = ./templates/2024-06-14;
+    description = "2024-06-14";
+  };
+  r-2024-08-19 = {
+    path = ./templates/2024-08-19;
+    description = "2024-08-19";
+  };
+  r-2024-10-01 = {
+    path = ./templates/2024-10-01;
+    description = "2024-10-01";
+  };
+  r-2024-12-14 = {
+    path = ./templates/2024-12-14;
+    description = "2024-12-14";
+  };
+  r-2025-01-14 = {
+    path = ./templates/2025-01-14;
+    description = "2025-01-14";
+  };
+  r-2025-01-24 = {
+    path = ./templates/2025-01-24;
+    description = "2025-01-24";
+  };
+  r-2025-01-27 = {
+    path = ./templates/2025-01-27;
+    description = "2025-01-27";
+  };
+  r-2025-02-03 = {
+    path = ./templates/2025-02-03;
+    description = "2025-02-03";
+  };
+  r-2025-02-10 = {
+    path = ./templates/2025-02-10;
+    description = "2025-02-10";
+  };
+  r-3_5_3 = {
+    path = ./templates/3.5.3;
+    description = "3.5.3";
+  };
+  r-3_6_0 = {
+    path = ./templates/3.6.0;
+    description = "3.6.0";
+  };
+  r-3_6_1 = {
+    path = ./templates/3.6.1;
+    description = "3.6.1";
+  };
+  r-3_6_2 = {
+    path = ./templates/3.6.2;
+    description = "3.6.2";
+  };
+  r-3_6_3 = {
+    path = ./templates/3.6.3;
+    description = "3.6.3";
+  };
+  r-4_0_0 = {
+    path = ./templates/4.0.0;
+    description = "4.0.0";
+  };
+  r-4_0_1 = {
+    path = ./templates/4.0.1;
+    description = "4.0.1";
+  };
+  r-4_0_2 = {
+    path = ./templates/4.0.2;
+    description = "4.0.2";
+  };
+  r-4_0_3 = {
+    path = ./templates/4.0.3;
+    description = "4.0.3";
+  };
+  r-4_0_4 = {
+    path = ./templates/4.0.4;
+    description = "4.0.4";
+  };
+  r-4_0_5 = {
+    path = ./templates/4.0.5;
+    description = "4.0.5";
+  };
+  r-4_1_0 = {
+    path = ./templates/4.1.0;
+    description = "4.1.0";
+  };
+  r-4_1_1 = {
+    path = ./templates/4.1.1;
+    description = "4.1.1";
+  };
+  r-4_1_2 = {
+    path = ./templates/4.1.2;
+    description = "4.1.2";
+  };
+  r-4_1_3 = {
+    path = ./templates/4.1.3;
+    description = "4.1.3";
+  };
+  r-4_2_0 = {
+    path = ./templates/4.2.0;
+    description = "4.2.0";
+  };
+  r-4_2_1 = {
+    path = ./templates/4.2.1;
+    description = "4.2.1";
+  };
+  r-4_2_2 = {
+    path = ./templates/4.2.2;
+    description = "4.2.2";
+  };
+  r-4_2_3 = {
+    path = ./templates/4.2.3;
+    description = "4.2.3";
+  };
+  r-4_3_0 = {
+    path = ./templates/4.3.0;
+    description = "4.3.0";
+  };
+  r-4_3_1 = {
+    path = ./templates/4.3.1;
+    description = "4.3.1";
+  };
+  r-4_3_2 = {
+    path = ./templates/4.3.2;
+    description = "4.3.2";
+  };
+  r-4_3_3 = {
+    path = ./templates/4.3.3;
+    description = "4.3.3";
+  };
+  r-4_4_0 = {
+    path = ./templates/4.4.0;
+    description = "4.4.0";
+  };
+  r-4_4_1 = {
+    path = ./templates/4.4.1;
+    description = "4.4.1";
+  };
+  r-4_4_2 = {
+    path = ./templates/4.4.2;
+    description = "4.4.2";
+  };
   r-radian_vscode = {
     path = ./templates/radian_vscode;
     description = "radian_vscode";
---
+++ templates/2022-04-19/flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "A Nix-flake-based R development environment";
+  inputs = {
+    nixpkgs = {
+      url = "https://github.com/rstats-on-nix/nixpkgs/archive/2022-04-19.tar.gz";
+    };
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      installed_r_pkgs =
+        p: with p; [
+          data_table
+          ggplot2
+          A3
+        ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          }
+        );
+
+    in
+    {
+      overlays.default = final: prev: {
+        radianEnv = prev.radianWrapper.override {
+          packages = installed_r_pkgs prev.rPackages;
+        };
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+
+            LOCALE_ARCHIVE =
+              if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+            LANG = "en_US.UTF-8";
+            LC_ALL = "en_US.UTF-8";
+            LC_TIME = "en_US.UTF-8";
+            LC_MONETARY = "en_US.UTF-8";
+            LC_PAPER = "en_US.UTF-8";
+            LC_MEASUREMENT = "en_US.UTF-8";
+            packages = with pkgs; [
+              glibcLocales
+              nix
+              radianEnv
+            ];
+            shellHook =
+              let
+                rtermsys = if pkgs.stdenv.isLinux then "linux" else "mac";
+              in
+              ''
+                mkdir -p .vscode
+                tr -d '{}' < .vscode/settings.json > .vscode/tmp.json
+                sed -i 's/^ */   /' .vscode/tmp.json
+                sed -i '/"r\.rterm/d' .vscode/tmp.json 
+                echo '   "r.rterm.${rtermsys}": "${pkgs.radianEnv}/bin/radian",' >> .vscode/tmp.json
+                echo '{' > .vscode/settings.json
+                cat .vscode/tmp.json | sort -u | sed '/^ *$/d' >> .vscode/settings.json
+                echo '}' >> .vscode/settings.json
+                rm .vscode/tmp.json
+              '';
+
+          };
+
+        }
+      );
+    };
+}

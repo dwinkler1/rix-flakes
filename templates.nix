c3530ce0b1b4869c0ae645c4907267e98a716e64
dwinkler1 <dwinkler1@users.noreply.github.com>
2025-03-03 20:40:00 +0000

    Commit from GitHub Actions (Recreate R flakes)

--- templates.nix
+++ templates.nix
@@ -279,6 +279,470 @@
     path = ./templates/bleeding-edge;
     description = "bleeding-edge";
   };
+  r-daily-2019-03-14 = {
+    path = ./templates/daily-2019-03-14;
+    description = "daily-2019-03-14";
+  };
+  r-daily-2019-05-05 = {
+    path = ./templates/daily-2019-05-05;
+    description = "daily-2019-05-05";
+  };
+  r-daily-2019-05-06 = {
+    path = ./templates/daily-2019-05-06;
+    description = "daily-2019-05-06";
+  };
+  r-daily-2019-05-07 = {
+    path = ./templates/daily-2019-05-07;
+    description = "daily-2019-05-07";
+  };
+  r-daily-2019-07-22 = {
+    path = ./templates/daily-2019-07-22;
+    description = "daily-2019-07-22";
+  };
+  r-daily-2019-12-19 = {
+    path = ./templates/daily-2019-12-19;
+    description = "daily-2019-12-19";
+  };
+  r-daily-2020-03-12 = {
+    path = ./templates/daily-2020-03-12;
+    description = "daily-2020-03-12";
+  };
+  r-daily-2020-04-27 = {
+    path = ./templates/daily-2020-04-27;
+    description = "daily-2020-04-27";
+  };
+  r-daily-2020-06-07 = {
+    path = ./templates/daily-2020-06-07;
+    description = "daily-2020-06-07";
+  };
+  r-daily-2020-08-20 = {
+    path = ./templates/daily-2020-08-20;
+    description = "daily-2020-08-20";
+  };
+  r-daily-2020-10-30 = {
+    path = ./templates/daily-2020-10-30;
+    description = "daily-2020-10-30";
+  };
+  r-daily-2021-01-01 = {
+    path = ./templates/daily-2021-01-01;
+    description = "daily-2021-01-01";
+  };
+  r-daily-2021-02-26 = {
+    path = ./templates/daily-2021-02-26;
+    description = "daily-2021-02-26";
+  };
+  r-daily-2021-04-01 = {
+    path = ./templates/daily-2021-04-01;
+    description = "daily-2021-04-01";
+  };
+  r-daily-2021-05-29 = {
+    path = ./templates/daily-2021-05-29;
+    description = "daily-2021-05-29";
+  };
+  r-daily-2021-08-03 = {
+    path = ./templates/daily-2021-08-03;
+    description = "daily-2021-08-03";
+  };
+  r-daily-2021-10-28 = {
+    path = ./templates/daily-2021-10-28;
+    description = "daily-2021-10-28";
+  };
+  r-daily-2022-01-01 = {
+    path = ./templates/daily-2022-01-01;
+    description = "daily-2022-01-01";
+  };
+  r-daily-2022-01-16 = {
+    path = ./templates/daily-2022-01-16;
+    description = "daily-2022-01-16";
+  };
+  r-daily-2022-03-01 = {
+    path = ./templates/daily-2022-03-01;
+    description = "daily-2022-03-01";
+  };
+  r-daily-2022-04-19 = {
+    path = ./templates/daily-2022-04-19;
+    description = "daily-2022-04-19";
+  };
+  r-daily-2022-06-22 = {
+    path = ./templates/daily-2022-06-22;
+    description = "daily-2022-06-22";
+  };
+  r-daily-2022-08-22 = {
+    path = ./templates/daily-2022-08-22;
+    description = "daily-2022-08-22";
+  };
+  r-daily-2022-10-20 = {
+    path = ./templates/daily-2022-10-20;
+    description = "daily-2022-10-20";
+  };
+  r-daily-2022-12-20 = {
+    path = ./templates/daily-2022-12-20;
+    description = "daily-2022-12-20";
+  };
+  r-daily-2023-02-13 = {
+    path = ./templates/daily-2023-02-13;
+    description = "daily-2023-02-13";
+  };
+  r-daily-2023-04-01 = {
+    path = ./templates/daily-2023-04-01;
+    description = "daily-2023-04-01";
+  };
+  r-daily-2023-06-15 = {
+    path = ./templates/daily-2023-06-15;
+    description = "daily-2023-06-15";
+  };
+  r-daily-2023-08-15 = {
+    path = ./templates/daily-2023-08-15;
+    description = "daily-2023-08-15";
+  };
+  r-daily-2023-10-30 = {
+    path = ./templates/daily-2023-10-30;
+    description = "daily-2023-10-30";
+  };
+  r-daily-2023-12-30 = {
+    path = ./templates/daily-2023-12-30;
+    description = "daily-2023-12-30";
+  };
+  r-daily-2024-02-29 = {
+    path = ./templates/daily-2024-02-29;
+    description = "daily-2024-02-29";
+  };
+  r-daily-2024-04-29 = {
+    path = ./templates/daily-2024-04-29;
+    description = "daily-2024-04-29";
+  };
+  r-daily-2024-06-14 = {
+    path = ./templates/daily-2024-06-14;
+    description = "daily-2024-06-14";
+  };
+  r-daily-2024-08-19 = {
+    path = ./templates/daily-2024-08-19;
+    description = "daily-2024-08-19";
+  };
+  r-daily-2024-10-01 = {
+    path = ./templates/daily-2024-10-01;
+    description = "daily-2024-10-01";
+  };
+  r-daily-2024-12-14 = {
+    path = ./templates/daily-2024-12-14;
+    description = "daily-2024-12-14";
+  };
+  r-daily-2024-12-15 = {
+    path = ./templates/daily-2024-12-15;
+    description = "daily-2024-12-15";
+  };
+  r-daily-2024-12-16 = {
+    path = ./templates/daily-2024-12-16;
+    description = "daily-2024-12-16";
+  };
+  r-daily-2024-12-17 = {
+    path = ./templates/daily-2024-12-17;
+    description = "daily-2024-12-17";
+  };
+  r-daily-2024-12-18 = {
+    path = ./templates/daily-2024-12-18;
+    description = "daily-2024-12-18";
+  };
+  r-daily-2024-12-19 = {
+    path = ./templates/daily-2024-12-19;
+    description = "daily-2024-12-19";
+  };
+  r-daily-2024-12-20 = {
+    path = ./templates/daily-2024-12-20;
+    description = "daily-2024-12-20";
+  };
+  r-daily-2024-12-21 = {
+    path = ./templates/daily-2024-12-21;
+    description = "daily-2024-12-21";
+  };
+  r-daily-2024-12-22 = {
+    path = ./templates/daily-2024-12-22;
+    description = "daily-2024-12-22";
+  };
+  r-daily-2024-12-23 = {
+    path = ./templates/daily-2024-12-23;
+    description = "daily-2024-12-23";
+  };
+  r-daily-2024-12-24 = {
+    path = ./templates/daily-2024-12-24;
+    description = "daily-2024-12-24";
+  };
+  r-daily-2024-12-25 = {
+    path = ./templates/daily-2024-12-25;
+    description = "daily-2024-12-25";
+  };
+  r-daily-2024-12-26 = {
+    path = ./templates/daily-2024-12-26;
+    description = "daily-2024-12-26";
+  };
+  r-daily-2024-12-27 = {
+    path = ./templates/daily-2024-12-27;
+    description = "daily-2024-12-27";
+  };
+  r-daily-2024-12-28 = {
+    path = ./templates/daily-2024-12-28;
+    description = "daily-2024-12-28";
+  };
+  r-daily-2024-12-29 = {
+    path = ./templates/daily-2024-12-29;
+    description = "daily-2024-12-29";
+  };
+  r-daily-2024-12-30 = {
+    path = ./templates/daily-2024-12-30;
+    description = "daily-2024-12-30";
+  };
+  r-daily-2024-12-31 = {
+    path = ./templates/daily-2024-12-31;
+    description = "daily-2024-12-31";
+  };
+  r-daily-2025-01-01 = {
+    path = ./templates/daily-2025-01-01;
+    description = "daily-2025-01-01";
+  };
+  r-daily-2025-01-02 = {
+    path = ./templates/daily-2025-01-02;
+    description = "daily-2025-01-02";
+  };
+  r-daily-2025-01-03 = {
+    path = ./templates/daily-2025-01-03;
+    description = "daily-2025-01-03";
+  };
+  r-daily-2025-01-04 = {
+    path = ./templates/daily-2025-01-04;
+    description = "daily-2025-01-04";
+  };
+  r-daily-2025-01-05 = {
+    path = ./templates/daily-2025-01-05;
+    description = "daily-2025-01-05";
+  };
+  r-daily-2025-01-06 = {
+    path = ./templates/daily-2025-01-06;
+    description = "daily-2025-01-06";
+  };
+  r-daily-2025-01-07 = {
+    path = ./templates/daily-2025-01-07;
+    description = "daily-2025-01-07";
+  };
+  r-daily-2025-01-08 = {
+    path = ./templates/daily-2025-01-08;
+    description = "daily-2025-01-08";
+  };
+  r-daily-2025-01-09 = {
+    path = ./templates/daily-2025-01-09;
+    description = "daily-2025-01-09";
+  };
+  r-daily-2025-01-10 = {
+    path = ./templates/daily-2025-01-10;
+    description = "daily-2025-01-10";
+  };
+  r-daily-2025-01-11 = {
+    path = ./templates/daily-2025-01-11;
+    description = "daily-2025-01-11";
+  };
+  r-daily-2025-01-12 = {
+    path = ./templates/daily-2025-01-12;
+    description = "daily-2025-01-12";
+  };
+  r-daily-2025-01-13 = {
+    path = ./templates/daily-2025-01-13;
+    description = "daily-2025-01-13";
+  };
+  r-daily-2025-01-14 = {
+    path = ./templates/daily-2025-01-14;
+    description = "daily-2025-01-14";
+  };
+  r-daily-2025-01-15 = {
+    path = ./templates/daily-2025-01-15;
+    description = "daily-2025-01-15";
+  };
+  r-daily-2025-01-16 = {
+    path = ./templates/daily-2025-01-16;
+    description = "daily-2025-01-16";
+  };
+  r-daily-2025-01-17 = {
+    path = ./templates/daily-2025-01-17;
+    description = "daily-2025-01-17";
+  };
+  r-daily-2025-01-18 = {
+    path = ./templates/daily-2025-01-18;
+    description = "daily-2025-01-18";
+  };
+  r-daily-2025-01-19 = {
+    path = ./templates/daily-2025-01-19;
+    description = "daily-2025-01-19";
+  };
+  r-daily-2025-01-20 = {
+    path = ./templates/daily-2025-01-20;
+    description = "daily-2025-01-20";
+  };
+  r-daily-2025-01-21 = {
+    path = ./templates/daily-2025-01-21;
+    description = "daily-2025-01-21";
+  };
+  r-daily-2025-01-22 = {
+    path = ./templates/daily-2025-01-22;
+    description = "daily-2025-01-22";
+  };
+  r-daily-2025-01-23 = {
+    path = ./templates/daily-2025-01-23;
+    description = "daily-2025-01-23";
+  };
+  r-daily-2025-01-24 = {
+    path = ./templates/daily-2025-01-24;
+    description = "daily-2025-01-24";
+  };
+  r-daily-2025-01-25 = {
+    path = ./templates/daily-2025-01-25;
+    description = "daily-2025-01-25";
+  };
+  r-daily-2025-01-26 = {
+    path = ./templates/daily-2025-01-26;
+    description = "daily-2025-01-26";
+  };
+  r-daily-2025-01-27 = {
+    path = ./templates/daily-2025-01-27;
+    description = "daily-2025-01-27";
+  };
+  r-daily-2025-01-28 = {
+    path = ./templates/daily-2025-01-28;
+    description = "daily-2025-01-28";
+  };
+  r-daily-2025-01-29 = {
+    path = ./templates/daily-2025-01-29;
+    description = "daily-2025-01-29";
+  };
+  r-daily-2025-01-30 = {
+    path = ./templates/daily-2025-01-30;
+    description = "daily-2025-01-30";
+  };
+  r-daily-2025-01-31 = {
+    path = ./templates/daily-2025-01-31;
+    description = "daily-2025-01-31";
+  };
+  r-daily-2025-02-01 = {
+    path = ./templates/daily-2025-02-01;
+    description = "daily-2025-02-01";
+  };
+  r-daily-2025-02-02 = {
+    path = ./templates/daily-2025-02-02;
+    description = "daily-2025-02-02";
+  };
+  r-daily-2025-02-03 = {
+    path = ./templates/daily-2025-02-03;
+    description = "daily-2025-02-03";
+  };
+  r-daily-2025-02-04 = {
+    path = ./templates/daily-2025-02-04;
+    description = "daily-2025-02-04";
+  };
+  r-daily-2025-02-05 = {
+    path = ./templates/daily-2025-02-05;
+    description = "daily-2025-02-05";
+  };
+  r-daily-2025-02-06 = {
+    path = ./templates/daily-2025-02-06;
+    description = "daily-2025-02-06";
+  };
+  r-daily-2025-02-07 = {
+    path = ./templates/daily-2025-02-07;
+    description = "daily-2025-02-07";
+  };
+  r-daily-2025-02-08 = {
+    path = ./templates/daily-2025-02-08;
+    description = "daily-2025-02-08";
+  };
+  r-daily-2025-02-09 = {
+    path = ./templates/daily-2025-02-09;
+    description = "daily-2025-02-09";
+  };
+  r-daily-2025-02-10 = {
+    path = ./templates/daily-2025-02-10;
+    description = "daily-2025-02-10";
+  };
+  r-daily-2025-02-11 = {
+    path = ./templates/daily-2025-02-11;
+    description = "daily-2025-02-11";
+  };
+  r-daily-2025-02-12 = {
+    path = ./templates/daily-2025-02-12;
+    description = "daily-2025-02-12";
+  };
+  r-daily-2025-02-13 = {
+    path = ./templates/daily-2025-02-13;
+    description = "daily-2025-02-13";
+  };
+  r-daily-2025-02-14 = {
+    path = ./templates/daily-2025-02-14;
+    description = "daily-2025-02-14";
+  };
+  r-daily-2025-02-15 = {
+    path = ./templates/daily-2025-02-15;
+    description = "daily-2025-02-15";
+  };
+  r-daily-2025-02-16 = {
+    path = ./templates/daily-2025-02-16;
+    description = "daily-2025-02-16";
+  };
+  r-daily-2025-02-17 = {
+    path = ./templates/daily-2025-02-17;
+    description = "daily-2025-02-17";
+  };
+  r-daily-2025-02-18 = {
+    path = ./templates/daily-2025-02-18;
+    description = "daily-2025-02-18";
+  };
+  r-daily-2025-02-19 = {
+    path = ./templates/daily-2025-02-19;
+    description = "daily-2025-02-19";
+  };
+  r-daily-2025-02-20 = {
+    path = ./templates/daily-2025-02-20;
+    description = "daily-2025-02-20";
+  };
+  r-daily-2025-02-21 = {
+    path = ./templates/daily-2025-02-21;
+    description = "daily-2025-02-21";
+  };
+  r-daily-2025-02-22 = {
+    path = ./templates/daily-2025-02-22;
+    description = "daily-2025-02-22";
+  };
+  r-daily-2025-02-23 = {
+    path = ./templates/daily-2025-02-23;
+    description = "daily-2025-02-23";
+  };
+  r-daily-2025-02-24 = {
+    path = ./templates/daily-2025-02-24;
+    description = "daily-2025-02-24";
+  };
+  r-daily-2025-02-25 = {
+    path = ./templates/daily-2025-02-25;
+    description = "daily-2025-02-25";
+  };
+  r-daily-2025-02-26 = {
+    path = ./templates/daily-2025-02-26;
+    description = "daily-2025-02-26";
+  };
+  r-daily-2025-02-27 = {
+    path = ./templates/daily-2025-02-27;
+    description = "daily-2025-02-27";
+  };
+  r-daily-2025-02-28 = {
+    path = ./templates/daily-2025-02-28;
+    description = "daily-2025-02-28";
+  };
+  r-daily-2025-03-01 = {
+    path = ./templates/daily-2025-03-01;
+    description = "daily-2025-03-01";
+  };
+  r-daily-2025-03-02 = {
+    path = ./templates/daily-2025-03-02;
+    description = "daily-2025-03-02";
+  };
+  r-daily-2025-03-03 = {
+    path = ./templates/daily-2025-03-03;
+    description = "daily-2025-03-03";
+  };
   r-default = {
     path = ./templates/default;
     description = "default";

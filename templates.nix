39896124da453149b9430193c34b2c5446e597b9
dwinkler1 <dwinkler1@users.noreply.github.com>
2025-04-15 08:44:23 +0000

    Commit from GitHub Actions (Recreate R flakes)

--- templates.nix
+++ templates.nix
@@ -187,6 +187,10 @@
     path = ./templates/2025-04-11;
     description = "2025-04-11";
   };
+  r-2025-04-14 = {
+    path = ./templates/2025-04-14;
+    description = "2025-04-14";
+  };
   r-3_5_3 = {
     path = ./templates/3.5.3;
     description = "3.5.3";

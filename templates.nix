c5baba37926bfba948c4d2e89d3da05b5ae6ce9b
dwinkler1 <dwinkler1@users.noreply.github.com>
2026-08-25 01:57:13 +0000

    Commit from GitHub Actions (Recreate R flakes)

--- templates.nix
+++ templates.nix
@@ -3151,6 +3151,10 @@
     path = ./templates/daily-2026-08-24;
     description = "daily-2026-08-24";
   };
+  r-daily-2026-08-25 = {
+    path = ./templates/daily-2026-08-25;
+    description = "daily-2026-08-25";
+  };
   r-default = {
     path = ./templates/default;
     description = "default";
